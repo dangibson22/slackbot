d70ad42310b8d86f809789d44db6b8e46ce8f102
dangibson22 <dan.w.gibson.22@gmail.com>
2022-01-31 22:28:56 -0500

    Working on google API access

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-test_var = os.getenv('test')
-print(f'env test: {test_var}')
+    app = App(token=os.getenv('bot_token'))
 
-app = App(token=os.getenv('bot_token'))
+    test_channel_id="C02R31VRR9C"
 
-test_channel_id="C02R31VRR9C"
-
-result = app.client.chat_postMessage(
-    channel=test_channel_id,
-    text="Hello world!"
-)
+    app.client.chat_postMessage(
+        channel=test_channel_id,
+        text="Hello world!"
+    )
 
 if __name__ == "__main__":
-    SocketModeHandler(app, os.getenv('app_token')).start()
+    # SocketModeHandler(app, os.getenv('app_token')).start()
+    main()
